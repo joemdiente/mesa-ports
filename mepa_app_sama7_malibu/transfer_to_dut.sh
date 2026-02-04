98b0467f1e3abffab3ae498752d9e523a2d9760d
Joemel John Diente <joemdiente@gmail.com>
2026-02-05 01:47:43 +0800

    mepa_app_sama7_malibu: add switch to build phy_demo_appl. No spi HAL yet (WIP); new-api, use manual build in building sw-mepa libs.

--- mepa_app_sama7_malibu/transfer_to_dut.sh
+++ mepa_app_sama7_malibu/transfer_to_dut.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 #clear keys
-if [ "$1" = "clear_keys" ];
+if [ "$1" = "clear-keys" ];
     then
         echo "Clearing keys..."
         ssh-keygen -f '/home/test/.ssh/known_hosts' -R '192.168.137.221'
-        echo "Make sure to answer \"yes\"."
+        echo "========================================================================================"
+        echo "Make sure to answer \"yes\" when asked to continue connecting..."
+        echo "If target is asking for password, make sure host .pub key is in target's authorized_keys"
+        echo "========================================================================================"
+        echo "\r\n"
 fi
 
 #transfer files to DUT
-echo "Transfer files to DUT"
+echo "Transfer files to DUT....\r\n"
 scp ./malibu10_sanity_check_app root@192.168.137.221:~/
+echo "\r\nDone."
