4d8260fb22e760699bddabc06744471119bc18eb
Joemel John Diente <joemdiente@gmail.com>
2026-04-03 17:14:07 +0800

    mepa_app_sama7: detect 1G Cu SFP before proceeding with demo.

--- mepa_app_sama7/mepa_app_port_linkup/mepa_app_port_linkup.c
+++ mepa_app_sama7/mepa_app_port_linkup/mepa_app_port_linkup.c
@@ -80,10 +80,7 @@ int mepa_app_sample_appl(appl_inst_t *inst)
         if (mepa_conf_get(inst->phy[port_no], &conf) < 0) {
             printf("Failed to get current port configuration for port %d\r\n", port_no);
         }
-    }
-
-    for (port_no = 0; port_no < MALIBU_EVB_PORT_COUNT; port_no++) {
-
+        
         // PHY Configuration
         conf.speed = MESA_SPEED_10G;
         conf.conf_10g.oper_mode = MEPA_PHY_LAN_MODE;
@@ -115,16 +112,34 @@ int mepa_app_sample_appl(appl_inst_t *inst)
      * configuring GPIO (or I2C) else, gpio or i2c will NOT work.
      * eg, i2c read will always return 0x00. 
      */ 
+    printf("\r\n MEPA GPIO and I2C Configuration\r\n");
     for (port_no = 0; port_no < MALIBU_EVB_PORT_COUNT; port_no++) {
-        printf("Configuring GPIOs for port %d\n", port_no);
-        ret = appl_malibu_gpio_conf(inst, port_no);
-        printf("appl_malibu_gpio_conf: rc: %d\n\n", ret);
+        printf("Port %d gpio configuration %s\r\n", port_no, appl_malibu_gpio_conf(inst, port_no)? "failed" : "success");
     }
-    exit(EXIT_SUCCESS);
 
-    // 5. Check Link Status
+    // 5. Wait for 1G CU SFP Insertion.
+    uint8_t data = 0;
+    port_no = 2;
+    printf(" Waiting to insert SFP/SFP+ on P1.\r\n");
+    while (1) {
+        mepa_i2c_read(inst->phy[port_no], 0, 0x0, 0, 0, 1, &data); // Read address 0x0 for 0x3 value.
+        if (data == 0x3) {
+            printf("SFP/SFP+ detected on P1.\r\n");
 
+            // Then detect if 1000Base-T SFP.
+            mepa_i2c_read(inst->phy[port_no], 0, 0x6, 0, 0, 1, &data); //Read address 0x6 bit 3 for 1000Base-T
+            printf("Ethernet Compliance Code: 0x%02x\r\n", data);
+            if ((data & 0b1000) == 0b1000) {
+                printf("1000Base-T SFP/SFP+ detected on P1.\r\n");
+                break;
+            }
+        }
+
+        sleep(1);
+    }
     
+    // 6. Monitor Link Status and Print it.
+
     return ret;
 }
 
